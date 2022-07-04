c782afefadb7216f9f9cbe859e98ff132a80d923
Joao <jean_janduy@yahoo.com.br>
2022-07-04 10:45:46 -0300

    Implantando contador com addEventListener e Feito Dicas

--- introducao-ao-javascript/contador/assets/scripts.js
+++ introducao-ao-javascript/contador/assets/scripts.js
@@ -1,17 +1,24 @@
-let count = 0;
+ let count = 0;
 
 const CURRENT_NUMBER = document.getElementById('currentNumber');
 
-function increment() {
-	count++;
-	CURRENT_NUMBER.innerHTML = count;
+function increment() { 
+	if(count < 10){
+		count++;
+		CURRENT_NUMBER.innerHTML = count;
+		if (count>=0){CURRENT_NUMBER.style.color="black"}
+	}
 }
 
 function decrement() {
-	count--;
-	CURRENT_NUMBER.innerHTML = count;
+	if(count>-10){
+		count--;
+		CURRENT_NUMBER.innerHTML = count;
+		if (count<0){CURRENT_NUMBER.style.color="red"}
+	}
 }
 
-function test() {
-	kdowkdpo;
-}
+// Implementando com addEventListener
+document.getElementById("subtrair").addEventListener("click",decrement);
+document.getElementById("adicionar").addEventListener("click",increment);
+
